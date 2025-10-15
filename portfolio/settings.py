7bbc8703c13cba003aed5aeb841efa059161ab8d
sameerdhiman08-dev <sameerdhiman08@gmail.com>
2025-10-15 22:14:17 +0530

    Fixed static files for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+WHITENOISE_USE_FINDERS = True  
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
