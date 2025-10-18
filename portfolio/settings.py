01b39ada5726ae50d8bb87d86292e59e0ad6b01a
sameerdhiman08-dev <sameerdhiman08@gmail.com>
2025-10-18 22:13:30 +0530

    Fixed static files for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+       'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
