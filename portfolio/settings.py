316ed0a9a92105a9c1673fedc26570260d655425
sameerdhiman08-dev <sameerdhiman08@gmail.com>
2025-10-18 21:50:19 +0530

    Fixed static files for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,11 +28,11 @@ import dj_database_url  # ✅ for Render DB config
 SECRET_KEY =os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 # DEBUG
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://my-porfolio-pbdi.onrender.com']
+ALLOWED_HOSTS = ['my-porfolio-pbdi.onrender.com']
+
 
 # Application definition
 
